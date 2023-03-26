57dc51200aec2914c98c5586ec0a13905e436f8d
vashkevich-polina <127602760+vashkevich-polina@users.noreply.github.com>
2023-03-26 06:51:05 +0000

    Lesson2

--- Lesson2/Home/Program.cs
+++ Lesson2/Home/Program.cs
@@ -1,33 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Enter two numbers");
- String number1 = Console.ReadLine();
- bool success1 = float.TryParse(number1, out float num1);
+string number1 = Console.ReadLine();
+bool success1 = float.TryParse(number1, out float num1);
 
- String number2 = Console.ReadLine();
-  bool success2 = float.TryParse(number2, out float num2);
-  if(success2 && success1){
+string number2 = Console.ReadLine();
+bool success2 = float.TryParse(number2, out float num2);
 
-Console.WriteLine("What do you want to do with numbers: + , - , * , / ?");
-String operation = Console.ReadLine();
+if (success2 && success1)
+{
+    Console.WriteLine("What do you want to do with numbers: + , - , * , / ?");
+    string operation = Console.ReadLine();
+    float answer = 0;
+    if (operation == "+")
+    {
+        answer = num1 + num2;
 
-if (operation == "+") {
-    float answer= num1 + num2;
-    Console.WriteLine(" Answer = " +answer);
-}else {
-    if (operation == "-") {
-float answer= num1 - num2;
-Console.WriteLine(" Answer = " +answer);
-    } else {
-        if ( operation == "*"){
-           float answer= num1 * num2;
-Console.WriteLine(" Answer = " +answer); 
-        } else {
-            float answer= num1 / num2;
-Console.WriteLine(" Answer = " +answer);
-        }
+    }
+    else
+
+        if (operation == "-")
+    {
+        answer = num1 - num2;
+
+    }
+    else
+
+            if (operation == "*")
+    {
+        answer = num1 * num2;
+
+    }
+    else
+    {
+        answer = num1 / num2;
+
     }
 
+    Console.WriteLine(" Answer = " + answer);
+
 }
-} else {
+else
+{
     Console.WriteLine(" Not number");
 }
